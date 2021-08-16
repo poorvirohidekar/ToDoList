1c6f4684ca1446f73d1958d19865783232dc0641
poorvirohidekar <53335539+poorvirohidekar@users.noreply.github.com>
2021-08-16 15:27:22 +0530

    Add files via upload

---
+++ src/app/hero.ts
@@ -0,0 +1,6 @@
+export interface Hero {
+    id: number;
+    name: string;
+    phone: string;
+    address: string;
+  }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', phone: '13847374', address : 'gujurat'},
+  { id: 12, name: 'Narco', phone: '13847374', address : 'gujurat' },
+  { id: 13, name: 'Bombasto' , phone: '13847374', address : 'gujurat'},
+  { id: 14, name: 'Celeritas', phone: '13847374', address : 'gujurat' },
+  { id: 15, name: 'Magneta', phone: '13847374', address : 'gujurat' },
+  { id: 16, name: 'RubberMan' , phone: '13847374', address : 'gujurat'},
+  { id: 17, name: 'Dynama', phone: '13847374', address : 'gujurat' },
+  { id: 18, name: 'Dr IQ', phone: '13847374', address : 'gujurat' },
+  { id: 19, name: 'Magma', phone: '13847374', address : 'gujurat' },
+  { id: 20, name: 'Tornado', phone: '13847374', address : 'gujurat' }
+];
